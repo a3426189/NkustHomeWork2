26dd8a29823d5ee2912e45df168cfb4dd3d811b7
1106105347 <1106105347@nkust.edu.tw>
2021-04-14 16:13:14 +0800

    讀取與寫入(DBManager)

--- WebApplication/Controllers/ActivityController.cs
+++ WebApplication/Controllers/ActivityController.cs
@@ -4,20 +4,38 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Configuration;
+using WebApplication.Models;
 
 namespace WebApplication.Controllers
 {
+    
     public class ActivityController : Controller
     {
         public IActionResult Index()
         {
-            var service = new ConsoleApp.Services.ImportJsonService();
-            var filePath = ConsoleApp.Utils.FilePath.GetFullPath("高雄活動.txt");
-            List<Activity> datas = service.LoadFormFile(filePath);
+
+
+            DBManager DB = new DBManager();
+
+            //var service = new ConsoleApp.Services.ImportJsonService();
+            //var filePath = ConsoleApp.Utils.FilePath.GetFullPath("高雄活動.txt");
+            //List<Activity> datas = service.LoadFormFile(filePath);
+
+            List<Activity> datas = DB.GetActivities();
+            //InSertDatas(datas);
 
             //return Json(datas);
 
             return View(datas);
+        }
+        public void InSertDatas(List<Activity> datas)
+        {
+            DBManager DB = new DBManager();
+            foreach (var X in datas)
+            {
+                DB.NewActivity(X);
+            }
         }
     }
 }
---
+++ WebApplication/Models/DBManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using ConsoleApp.Models;
+using System.Data.SqlClient;
+
+namespace WebApplication.Models
+{
+    public class DBManager
+    {
+        
+        const string ConnStr = "Data Source=DESKTOP-C0IK21H;Initial Catalog=storeDB;Integrated Security=True";
+        public void NewActivity(Activity activity)
+        {
+            SqlConnection sqlConnection = new SqlConnection(ConnStr);
+            sqlConnection.Open();
+            SqlCommand cmd = new SqlCommand("insert into PrgData values(@ID, @NAME)", sqlConnection);
+            cmd.Parameters.AddWithValue("@ID", activity.PrgId);
+            cmd.Parameters.AddWithValue("@NAME", activity.PrgName);
+
+
+            cmd.ExecuteNonQuery();
+            sqlConnection.Close();
+        }
+        public List<Activity> GetActivities()
+        {
+            List<Activity> Activities = new List<Activity>();
+
+            SqlConnection sqlConnection = new SqlConnection(ConnStr);
+
+            SqlCommand sqlCommand = new SqlCommand("SELECT * FROM PrgData");
+            sqlCommand.Connection = sqlConnection;
+            sqlConnection.Open();
+
+            SqlDataReader reader = sqlCommand.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    Activity activity = new Activity
+                    {
+                        PrgId = reader.GetInt32(reader.GetOrdinal("PrgId")),
+                        PrgName = reader.GetString(reader.GetOrdinal("PrgName")),
+                    };
+                    Activities.Add(activity);
+                }
+            }
+            else
+            {
+                Console.WriteLine("資料庫為空！");
+            }
+            sqlConnection.Close();
+            return Activities;
+        }
+    }
+    
+    
+}
